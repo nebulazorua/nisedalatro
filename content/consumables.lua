6b24f5179d4e59c0b4af0b3616b1f48fc2f1ac7a
nebulazorua <12458328+nebulazorua@users.noreply.github.com>
2026-04-06 07:04:59 +0800

    fortune cookie changes

--- content/consumables.lua
+++ content/consumables.lua
@@ -4,37 +4,42 @@ SMODS.Sound {
 }
 
 
+---@param card? Card|table
+---@return number
+--- Returns the numerator used for The Fortune Cookie.
+--- Gets higher with each successful usage of the cookie
+function Nisedalatro.get_cookie_denominator(card)
+	local oddMult = G.GAME.COOKIES or 0; 
+	return card.ability.extra.odds * ((oddMult * 0.5) + 1);
+end
+
 SMODS.Consumable {
     key = 'fortune_cookie',
     set = 'Tarot',
     pos = { x = 2, y = 0 },
-    config = { extra = { odds = 25 , mult = 1.5} },
+    config = { extra = { odds = 30 , mult = 2} },
     loc_vars = function(self, info_queue, card)
-        local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds,
-            'nest_fortune_cookie')
+		local numerator, denominator = SMODS.get_probability_vars(card, 1, Nisedalatro.get_cookie_denominator(card), 'nest_fortune_cookie')
         local mult = card.ability.extra.mult
         return { vars = { numerator, denominator, mult }}
     end,
     use = function(self, card, area, copier)
-		if SMODS.pseudorandom_probability(card, "nest_fortune_cookie", 1, card.ability.extra.odds)then
+		local goated = SMODS.pseudorandom_probability(card, "nest_fortune_cookie", 1, Nisedalatro.get_cookie_denominator(card))
+		if goated then
 			play_sound("nest_goated_cookie")
-			local thingsToCheck = { jokers = G.jokers.cards, cons = G.consumeables.cards, shop = G.shop_jokers == nil and
-			{} or G.shop_jokers.cards }
-			for _, thing in next, thingsToCheck do
-				for it = 1, #thing do
-					local item = thing[it]
-					for k,v in pairs(item.ability) do
-						if type(v) == 'number' then
-							if v ~= 0 and (v ~= 1 or (k ~= 'x_chips' and k ~= 'x_mult'))then
-								v = v * card.ability.extra.mult
-								item.ability[k] = v
-							end
-						elseif k == 'extra' then
-							for k2,v2 in pairs(item.ability.extra) do
-								if type(v2) == 'number' then
-									v2 = v2 * card.ability.extra.mult
-									item.ability.extra[k2] = v2
-								end
+			G.GAME.COOKIES = (G.GAME.COOKIES or 0) + 1;
+			for _, item in next, G.jokers.cards do
+				for k,v in pairs(item.ability) do
+					if type(v) == 'number' then
+						if v ~= 0 and (v ~= 1 or (k ~= 'x_chips' and k ~= 'x_mult'))then
+							v = v * card.ability.extra.mult
+							item.ability[k] = v
+						end
+					elseif k == 'extra' then
+						for k2,v2 in pairs(item.ability.extra) do
+							if type(v2) == 'number' then
+								v2 = v2 * card.ability.extra.mult
+								item.ability.extra[k2] = v2
 							end
 						end
 					end
--- localization/en-us.lua
+++ localization/en-us.lua
@@ -15,10 +15,10 @@ return {
 			c_nest_fortune_cookie = {
 				name = "Fortune Cookie",
 				text = {
-					"{C:green}#1# in #2#{} chance to increase the values of",
-					"all currently {C:attention}held cards{}",
-					"and all {C:attention}items currently in shop{}",
-					"by {X:mult,C:white}X#3#{}"
+					"{C:green}#1# in #2#{} chance",
+					"to increase the values of",
+					"all owned {C:attention}jokers{} by {X:mult,C:white}X#3#{}",
+					"{C:inactive}(The odds get lower with each success!){}"
 				}
 			}
 		}
