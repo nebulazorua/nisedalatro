31d5c6378d48d2020eebe8e219efb72ae94a61df
nebulazorua <12458328+nebulazorua@users.noreply.github.com>
2026-04-07 02:24:23 +0800

    moved garg

--- content/jokers.lua
+++ content/jokers.lua
@@ -32,6 +32,95 @@ SMODS.Sound {
 	path = "gargasell3.ogg"
 }
 
+SMODS.Joker {
+	key = 'gargamel',
+	blueprint_compat = true,
+	atlas = "jokers",
+	pos = {
+		x = 0,
+		y = 0,
+	},
+	soul_pos = {
+		x = 1,
+		y = 0
+	},
+	rarity = 3,
+	cost = 6,
+	unlocked = false,
+	discovered = false,
+	config = {
+		extra = {
+			xmult = 1,
+			xmult_mod = 0.2,
+			played_clubs_this_hand = 0 -- for displaying funny message
+		}
+	},
+	locked_loc_vars = function(self, info_queue, card)
+		return { vars = { 10, localize("Clubs", "suits_singular") } }
+	end,
+
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.xmult_mod, card.ability.extra.xmult } }
+	end,
+	calculate = function(self, card, context)
+		if context.destroying_card and context.destroy_card:is_suit("Clubs") and not context.retrigger_joker then
+			return {
+				remove = true
+			}
+		elseif context.individual and not context.blueprint_card and context.cardarea == G.play then
+			if context.other_card:is_suit("Clubs") then
+				if (not context.other_card.ability.garga_got_this_one) then
+					context.other_card.ability.garga_got_this_one = true;
+					card.ability.extra.played_clubs_this_hand = card.ability.extra.played_clubs_this_hand + 1;
+				end
+
+				SMODS.scale_card(card, {
+					ref_table = card.ability.extra,
+					ref_value = "xmult",
+					scalar_value = "xmult_mod",
+					no_message = true
+				})
+
+				return {
+					message = localize("k_upgrade_ex"),
+					message_card = card,
+					colour = G.C.MULT
+				}
+			end
+		elseif context.joker_main then
+			local retrigger = context.retrigger_joker or context.blueprint_card
+			return {
+				xmult = card.ability.extra.xmult,
+				delay = 0.4,
+				func = card.ability.extra.played_clubs_this_hand > 0 and function()
+					card.ability.extra.played_clubs_this_hand = 0;
+					if not retrigger then
+						card_eval_status_text(card, 'extra', nil, nil, nil,
+							{ message = localize("k_nest_gargamel"), sound = "nest_gargamel_trigger" })
+					end
+				end or nil
+			}
+		end
+	end,
+	add_to_deck = function(self, card, from_debuff)
+		play_sound("nest_gargamel_obtain")
+	end,
+	remove_from_deck = function(self, card, from_debuff)
+		play_sound("nest_gargamel_die" .. pseudorandom("nest_gargasell", 1, 3))
+	end,
+	check_for_unlock = function(self, args)
+		if args.type == 'card_destroyed' then
+			if args.card:is_suit("Clubs") then
+				G.GAME.DESTROYED_CLUBS = (G.GAME.DESTROYED_CLUBS or 0) + 1;
+				return G.GAME.DESTROYED_CLUBS >= 10
+			end
+		end
+		return false;
+	end
+}
+
+-- Skibidi Toilet and Creeper
+
 SMODS.Joker { -- Skibidi Toilet, cannot be bought alongside Creeper [skibidi toilet or creepare!!!]
 	key ='skibidi_toilet',
 	blueprint_compat = true,
@@ -50,7 +139,7 @@ SMODS.Joker { -- Skibidi Toilet, cannot be bought alongside Creeper [skibidi toi
 	rarity = 'nest_epic',
 	cost = 10,
 	unlocked = true,
-	discovered = true,
+	discovered = false,
 	loc_vars = function (self, info_queue, card)
 		local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'nest_skibidi_toilet')
 		return { vars = {numerator, denominator,  card.ability.extra.xmult, localize("Flush", "poker_hands") } }
@@ -115,7 +204,7 @@ SMODS.Joker { -- Creeper, cannot be bought alongside Skibidi Toilet [skibidi toi
 	rarity = 'nest_epic',
 	cost = 10,
 	unlocked = true,
-	discovered = true,
+	discovered = false,
 	config = {
 		extra = {
 			xmult = 2.5,
@@ -143,90 +232,3 @@ SMODS.Joker { -- Creeper, cannot be bought alongside Skibidi Toilet [skibidi toi
 		return not G.GAME.SKIBIDI_GREED
 	end
 }
-
-SMODS.Joker {
-	key ='gargamel',
-	blueprint_compat = true,
-	atlas = "jokers",
-	pos = {
-		x = 0,
-		y = 0,
-	},
-	soul_pos = {
-		x = 1,
-		y = 0
-	},
-	rarity = 3,
-	cost = 6,
-	unlocked = false,
-	discovered = false,
-	config = {
-		extra = {
-			xmult = 1,
-			xmult_mod = 0.2,
-			played_clubs_this_hand = 0 -- for displaying funny message
-		}
-	},
-	locked_loc_vars = function(self, info_queue, card)
-        return { vars = { 10, localize("Clubs", "suits_singular") } }
-    end,
-
-	loc_vars = function (self, info_queue, card)
-		return { vars = { card.ability.extra.xmult_mod, card.ability.extra.xmult } }
-	end,
-	calculate = function (self, card, context)
-		if context.destroying_card and context.destroy_card:is_suit("Clubs") and not context.retrigger_joker then
-			return {
-				remove = true
-			}
-		elseif context.individual and not context.blueprint_card and context.cardarea == G.play then
-			if context.other_card:is_suit("Clubs") then
-				if(not context.other_card.ability.garga_got_this_one)then
-					context.other_card.ability.garga_got_this_one = true;
-					card.ability.extra.played_clubs_this_hand = card.ability.extra.played_clubs_this_hand + 1;
-				end
-
-				SMODS.scale_card(card, {
-					ref_table = card.ability.extra,
-					ref_value = "xmult",
-					scalar_value = "xmult_mod",
-					no_message = true
-				})
-
-				return {
-					message = localize("k_upgrade_ex"),
-					message_card = card,
-					colour = G.C.MULT
-				}
-			end
-		elseif context.joker_main then
-			local retrigger = context.retrigger_joker or context.blueprint_card
-			return {
-				xmult = card.ability.extra.xmult,
-				delay = 0.4,
-				func = card.ability.extra.played_clubs_this_hand > 0 and function ()
-					card.ability.extra.played_clubs_this_hand = 0;
-					if not retrigger then
-						card_eval_status_text(card, 'extra', nil, nil, nil, { message = localize("k_nest_gargamel"), sound = "nest_gargamel_trigger" })
-					end
-				end or nil
-			}
-		end
-	end,
-	add_to_deck = function (self, card, from_debuff)
-		play_sound("nest_gargamel_obtain")
-	end,
-	remove_from_deck =function (self, card, from_debuff)
-		play_sound("nest_gargamel_die" .. pseudorandom("nest_gargasell", 1, 3))
-	end,
-	check_for_unlock = function (self, args)
-		if args.type == 'card_destroyed' then
-			if args.card:is_suit("Clubs")then
-				G.GAME.DESTROYED_CLUBS = (G.GAME.DESTROYED_CLUBS or 0) + 1;
-				return G.GAME.DESTROYED_CLUBS >= 10
-			end
-		end
-		return false;
-	end
-}
-
